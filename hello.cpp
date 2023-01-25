8033b7661ea6bd86e250c590bab7f4ec77dbb906
zaraamerr <zaraa.amerr@gmail.com>
2023-01-24 20:38:10 -0500

    did problem 53

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+//Name:  Zara Amer
+//Email:  zara.amer22@myhunter.cuny.edu
+//Date:  January 2023
+//My first program in C++
+
+#include <iostream>        //The built-in library for input/output
+using namespace std;       //The names of standard definitions
+
+int main()                 //C++ programs all have a main() function
+{
+  cout << "Hello, World!\n"; //Print "Hello, World!" to the terminal
+  cout << "C++ is fun!";
+  return 0;                //Standard way to end function
+}
