1498d7bdef1ae69dc96065f90771ba4d532d1208
Zara Amer <110066582+zaraamerr@users.noreply.github.com>
2023-01-25 04:37:32 +0000

    fixed error in comments

--- 54.cpp
+++ 54.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 //Prints "10" 6 times in one row for 10 rows.
-int main(){ // built in library for input/output
+int main(){
     for (int i = 0; i < 10; i++){ //starts at 0, checks if rows are < 10, if true it executes the loop until there are 10 rows. if false it does not execute the loop.
         for (int j = 0; j < 6; j++){ //starts at 0, checks if "10" is printed < 6 times, if true it keeps printing "10" within the row until there are 6 "10"s. if false, it does not print "10" in the same row again.
             cout << "10"; //output is 10
