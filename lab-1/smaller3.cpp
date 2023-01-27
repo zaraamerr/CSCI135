7867bcb9a7062c8949ef9e12021162a3c07a2d8f
zaraamerr <zaraa.amerr@gmail.com>
2023-01-26 21:44:47 -0500

    did task b of lab 1

--- lab-1/smaller3.cpp
+++ lab-1/smaller3.cpp
@@ -11,17 +11,20 @@ int main(){
     int first; //var that will store the first int that the user inputs
     int second; //var that will store the second int that the user inputs
     int third; //var that will store the third int that the user inputs
+    int smallest; //var that will store the smallest value
     cout << "Enter the first number: "; //asks user for first int
     cin >> first; //stores user input into first
     cout << "Enter the second number: "; //asks user for second int
     cin >> second; //stores user input into second
     cout << "Enter the third number: "; //asks user for third int
     cin >> third; //stores user input into third
-    if (first < second){ //if-statement checks if first int is less than second int
-        // cout << "The smaller of the two is " << first << endl; //if true, it prints this message
+    smallest= first;
+    if (second < smallest){ //if-statement checks if second int is less than the current smallest value
+        smallest= second; //if true, smallest is set to the value of the second int
+        
     }
-    else if (second < first){ //if the previous if-statement is false, it goes on to check if the second int is less than the first int
-        cout << "The smaller of the two is " << second << endl; //if true, it prints this message
+    if (third < smallest){ //if-statement checks if the third int is less than the current smallest value
+        smallest= third; //if true, smallest is set to the value of the third int
     }
     return 0;
 }
