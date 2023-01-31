60ad717ef422179643b129519a2eb57f348d70cf
zaraamerr <zaraa.amerr@gmail.com>
2023-01-31 11:42:01 -0500

    changed ints to doubles

--- gas.cpp
+++ gas.cpp
@@ -21,7 +21,7 @@ int main (){
     cin >> miles_per_gallon; //stores user input into var miles_per_gallon
     cout << "Enter the price of gas per gallon: " << endl; //asks user to input price of gas per gallon
     cin >> price; //stores user input into var price
-    cost_per_100_miles= (100.0 / miles_per_gallon * price_per_gallon); //set var cost_per_100 miles to a calculation that multiplies the price by 100 and divides it by how many miles can be traveled per gallon to determine what the cost is to travel 100 miles
+    cost_per_100_miles= (100.0 / miles_per_gallon * price); //set var cost_per_100 miles to a calculation that multiplies the price by 100 and divides it by how many miles can be traveled per gallon to determine what the cost is to travel 100 miles
     distance= (num_of_gallons*miles_per_gallon); //set var distance to a calculation that multiplies num_of_gallons by miles_per_gallon to determine how many miles the car can travel with the current amt of gas in its tank.
     cout << "The cost to travel 100 miles is " << cost_per_100_miles << " dollars and with " << num_of_gallons << " gallons of gas, the car can travel " << distance << " miles." << endl; //print output into terminal
     return 0; //end
