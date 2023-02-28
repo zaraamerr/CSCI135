c5fbac317b282738fb1294cfa849d3f3da19d36a
zaraamerr <zaraa.amerr@gmail.com>
2023-02-27 23:15:05 -0500

    try 2

--- projectaska.cpp
+++ projectaska.cpp
@@ -1,3 +1,63 @@
+//Name: Zara Amer
+//CSCI135
+//Date: Feb 27 2023
+//Professor Maryash
+//Task A Project 1
+
+// #include <iostream>
+// #include <fstream>
+// #include <string>
+
+// using namespace std;
+
+// const int g_MAX_WORDS = 1000;
+
+// string g_words[g_MAX_WORDS];
+// string g_pos[g_MAX_WORDS];
+// string g_definitions[g_MAX_WORDS];
+// int g_word_count = 0;
+
+// void readWords(string filename) {
+//     ifstream fin(filename);
+//     if (fin.fail()) {
+//         cerr << "File cannot be opened for reading." << endl;
+//         exit(1);
+//     }
+
+//     string junk;
+//     getline(fin, junk);
+
+//     string word;
+//     string pos;
+//     string definition;
+
+//     while (fin >> word >> pos) {
+//         getline(fin, definition);
+
+//         g_words[g_word_count] = word;
+//         g_pos[g_word_count] = pos.substr(0, pos.length());
+//         g_definitions[g_word_count] = definition.substr(1);
+
+//         g_word_count++;
+//         if (g_word_count >= g_MAX_WORDS) {
+//             cerr << "Exceeded maximum number of words" << endl;
+//             break;
+//         }
+//     }
+
+//     fin.close();
+// }
+
+// // int main() {
+// //     readWords("dict.txt");
+
+// //     for (int i = 0; i < g_word_count; i++) {
+// //         cout << g_words[i] << " " << g_pos[i] << g_definitions[i] << endl;
+// //     }
+
+// //     return 0;
+// // }
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -5,37 +65,32 @@
 using namespace std;
 
 const int g_MAX_WORDS = 1000;
-
-string g_words[g_MAX_WORDS];
-string g_pos[g_MAX_WORDS];
-string g_definitions[g_MAX_WORDS];
 int g_word_count = 0;
+string g_words[g_MAX_WORDS];
+string g_definitions[g_MAX_WORDS];
+string g_pos[g_MAX_WORDS];
 
 void readWords(string filename) {
     ifstream fin(filename);
-    if (fin.fail()) {
-        cerr << "File cannot be opened for reading." << endl;
+    if (!fin) {
+        cerr << "Error: Could not open file " << filename << endl;
         exit(1);
     }
 
+    string word, pos, definition;
     string junk;
     getline(fin, junk);
 
-    string word;
-    string pos;
-    string definition;
-
-    while (fin >> word >> pos) {
+    while (fin >> word >> pos >> junk) {
         getline(fin, definition);
-
         g_words[g_word_count] = word;
         g_pos[g_word_count] = pos.substr(0, pos.length());
         g_definitions[g_word_count] = definition.substr(1);
-
         g_word_count++;
+
         if (g_word_count >= g_MAX_WORDS) {
-            cerr << "Exceeded maximum number of words" << endl;
-            break;
+            cerr << "Error: Exceeded maximum number of words" << endl;
+            exit(1);
         }
     }
 
@@ -46,8 +101,9 @@ void readWords(string filename) {
 //     readWords("dict.txt");
 
 //     for (int i = 0; i < g_word_count; i++) {
-//         cout << g_words[i] << " " << g_pos[i] << g_definitions[i] << endl;
+//         cout << g_words[i] << " (" << g_pos[i] << "): " << g_definitions[i] << endl;
 //     }
 
 //     return 0;
 // }
+
