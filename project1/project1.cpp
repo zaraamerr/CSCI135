76017e9e9c1656d2f7781911416ef00036ffb075
zaraamerr <zaraa.amerr@gmail.com>
2023-03-02 00:38:21 -0500

    finale

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -8,6 +8,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdlib.h>
+
 
 using namespace std;
 
@@ -228,48 +230,208 @@ bool removeWord(string word) {
     return true;
 }
 
-
-int main() {
-    readWords("dict.txt");
-
-    for (int i = 0; i < g_word_count; i++) {
-        cout << g_words[i] << " " << g_pos[i] << ": " << g_definitions[i] << endl;
-    }
-    string word = "Grumpy";
-    string word2 = "Happy";
-
-    int index = getIndex(word);
-    cout << index << endl;
-    int index2 = getIndex(word2);
-    cout << index2 << endl;
-    string definition1 = getDefinition(word);
-    cout << definition1 << endl;
-    string definition2 = getDefinition(word2);
-    cout << definition2 << endl;
-    string pos1 = getPOS(word);
-    cout << pos1 << endl;
-    string pos2 = getPOS(word2);
-    cout << pos2 << endl; 
-    string prefix1= "Gru";
-    int count = countPrefix(prefix1);
-    cout << count << endl;
-    bool success = addWord("apple", "a fruit", "noun");
-
-    // Edit the word
-    success = editWord("apple", "a type of fruit", "noun");
-    if (success) {
-        cout << "Edit successful" << endl;
-    } else {
-        cout << "Edit failed" << endl;
-    }
-
-    // Remove the word
-    success = removeWord("apple");
-    if (success) {
-        cout << "Removal successful" << endl;
-    } else {
-        cout << "Removal failed" << endl;
-    }
-
-    return 0;
+string getRandomWord() {
+    srand((unsigned) time(NULL));
+    int index = rand() % g_word_count;
+    return g_words[index];
 }
+
+// game-loop for Hangman
+void gameLoop() {
+    int difficulty, tries;
+    string word, current;
+    char letter;
+    while (true) {
+        cout << "Welcome to Hangman!" << endl;
+        cout <<  "0. easy\n1. normal\n2. hard\n3. exit\nChoose a difficulty: ";
+        cin >> difficulty;
+        while (difficulty < 0 || difficulty > 3) {
+            cout <<  "Enough horseplay >_< !\n0. easy\n1. normal\n2. hard\n3. exit\nChoose a difficulty: ";
+            cin >> difficulty;
+        }
+        if (difficulty == 3) {
+            cout << "If you're hangry, go grab a bite! See what I did there?" << endl;
+            break;
+        }
+        word = getRandomWord();
+        current = maskWord(word);
+        tries = getTries(difficulty);
+        while (tries != 0) {
+            cout << "Life: ";
+            printAttempts(tries, difficulty);
+            cout << endl << "Word: "<< current << endl;
+            cout << "Enter a letter: ";
+            cin >> letter;
+            
+            if (!revealLetter(word, letter, current)) {
+                tries--;
+            }
+            if (current == word) {
+                break;
+            }
+            if (tries == 2) {
+                cout << "The part of speech of the word is "<< getPOS(word) << endl;
+            }
+            if (tries == 1) {
+                cout << "Definition of the word: " << getDefinition(word) << endl;
+            }
+        }
+        if (tries == 0) {
+            cout << "The word is \"" << word << "\". Better luck next time! You're getting the ..ahem.. hang of it." << endl;
+        }
+        else {
+            cout << "Congrats!!!" << endl;
+        }
+    }
+}
+
+/*
+    @param            :   The string with a word from the dictionary 
+    @return           :   string of "_" based on the number of
+                          characters in the given `word` 
+    @post             :   Return string of "_" based on the length
+                          of the given `word`.
+                          For example, if the word is "game", then
+                          the function would return "____". In other
+                          words, a string of four "_"s.
+*/
+
+string maskWord(string word) {
+    string masked = "";
+    for (int i = 0; i < word.length(); i++) { //for every letter in the word
+        masked += "_"; //add a space
+    }
+    return masked; //return the masked version of the word
+}
+
+/*
+    @param            :   The integer for the difficulty of the game
+                          (0 for easy, 1 for normal, and 2 for hard) 
+    @return           :   The number of tries given the `difficulty`
+                          (9 for easy, 7 for normal, and 5 for hard)
+    @post             :   Return the number of tries based on given
+                          difficulty (0-easy: 9 tries, 1-normal: 7
+                          tries, 2-Hard: 5 tries) 
+*/
+
+int getTries(int difficulty) {
+    if (difficulty == 0) { //easy
+        return 9; //9 tries
+    } else if (difficulty == 1) { //normal
+        return 7; //7 tries
+    } else if (difficulty == 2) { //hard
+        return 5; //5 tries
+    } else {
+        // invalid difficulty level, return -1 or throw an exception
+        return -1;
+    }
+}
+
+/*
+    @param tries      :   The integer for remaining tries 
+    @param difficulty :   The integer for the difficulty of the game 
+                          (0 for easy, 1 for normal, and 2 for hard) 
+    @post             :   prints the number of lives left and number
+                          of lives used using "O" and "X". DO NOT
+                          PRINT AN ENDLINE
+
+    For example : calling `printAttemps(2, 1)` would print "OOXXXXX". 
+                  Based on given `difficulty`, we know the total tries
+                  is 7 (from `getTries(1)`). Also, the player has 2
+                  `tries` remaining based on the given parameter.
+                  Therefore, the function prints two "0"s to indicate
+                  the remaining tries and 5 "X"s to indicate the tries
+                  that have been used (7-2=5)          
+*/
+
+void printAttempts(int tries, int difficulty) {
+    int totalTries = getTries(difficulty); //num of tries based on difficulty
+    for (int i = 0; i < totalTries; i++) { //iterates thru the tries
+        if (i < tries) { //index less than tries
+            cout << "O"; //O
+        } else {
+            cout << "X"; //X
+        }
+    }
+}
+
+/*
+    @param word       :   The string word from the dictionary
+    @param letter     :   The char letter that that will be revealed
+    @param(&) current :   The string representing a masked word
+    @return           :   `true` if the `letter` exists in `word`,
+                          otherwise return `false`  
+    @post             :   If the given `letter` exists in `word`
+                          reveal the `letter` in `current` masked word
+                          and return `true`. Otherwise, return `false`
+
+    For example : Let's say we have the following main function:
+                  int main(){
+                      string w = "g___";
+                      cout << revealLetter("good", 'o', "g___") << endl;
+                      cout <<  w << endl;
+                  }
+                  The first `cout` will print 1 because the letter 'o'
+                  exists in "good". Thus, the function returned `true`.
+                  The second `cout` will print "goo_". The variable `w`
+                  has been modified by the function to reveal all the
+                  `o`s in "good" resulting in "goo_"           
+*/
+
+bool revealLetter(string word, char letter, string& current) {
+    bool found = false;
+    for (int i = 0; i < word.length(); i++) {
+        if (word[i] == letter) {
+            current[i] = letter;
+            found = true;
+        }
+    }
+    return found;
+}
+
+
+
+// int main() {
+//     readWords("dict.txt");
+
+//     for (int i = 0; i < g_word_count; i++) {
+//         cout << g_words[i] << " " << g_pos[i] << ": " << g_definitions[i] << endl;
+//     }
+//     string word = "Grumpy";
+//     string word2 = "Happy";
+
+//     int index = getIndex(word);
+//     cout << index << endl;
+//     int index2 = getIndex(word2);
+//     cout << index2 << endl;
+//     string definition1 = getDefinition(word);
+//     cout << definition1 << endl;
+//     string definition2 = getDefinition(word2);
+//     cout << definition2 << endl;
+//     string pos1 = getPOS(word);
+//     cout << pos1 << endl;
+//     string pos2 = getPOS(word2);
+//     cout << pos2 << endl; 
+//     string prefix1= "Gru";
+//     int count = countPrefix(prefix1);
+//     cout << count << endl;
+//     bool success = addWord("apple", "a fruit", "noun");
+
+//     // Edit the word
+//     success = editWord("apple", "a type of fruit", "noun");
+//     if (success) {
+//         cout << "Edit successful" << endl;
+//     } else {
+//         cout << "Edit failed" << endl;
+//     }
+
+//     // Remove the word
+//     success = removeWord("apple");
+//     if (success) {
+//         cout << "Removal successful" << endl;
+//     } else {
+//         cout << "Removal failed" << endl;
+//     }
+
+//     return 0;
+// }
