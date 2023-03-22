c1de5ece78f3c824f513910a63108ae185346435
zaraamerr <zaraa.amerr@gmail.com>
2023-03-21 21:54:57 -0400

    hw 8.1

---
+++ filewrite.cpp
@@ -0,0 +1,38 @@
+//Zara Amer
+//CSCI 135
+//Professor Maryash
+//assignment e 8.1: Write a program that carries out the following tasks: Open a file with the name
+//hello.txt. Store the message “Hello, World!” in the file. Close the file. Open the same file again.
+//Read the message into a string variable and print it.
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Open the file for writing
+    std::ofstream outfile("hello.txt");
+    if (!outfile.is_open()) {
+        std::cerr << "Error opening file." << std::endl;
+        return 1;
+    }
+    outfile << "Hello, World!\n";
+    outfile.close();
+
+    // Open the file for reading
+    std::ifstream infile("hello.txt");
+    if (!infile.is_open()) {
+        std::cerr << "Error opening file." << std::endl;
+        return 1;
+    }
+    std::string message;
+    std::getline(infile, message);
+    infile.close();
+
+    // Print the message
+    std::cout << message << std::endl;
+
+    return 0;
+}
+
